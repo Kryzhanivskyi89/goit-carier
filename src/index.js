84f6c589e24614989dd43f561d1eb3baf690f056
Kryzhanivskyi89 <111448776+Kryzhanivskyi89@users.noreply.github.com>
2023-05-09 21:17:50 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./components/App";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.css";
+
+import { PersistGate } from "redux-persist/integration/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter basename="goit-carier">
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
